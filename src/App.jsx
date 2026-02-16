97f7394adc18896cf7011194f572154b3563a00c
Mahdi HamediKhah <m.k.h.13679@gmail.com>
2026-02-16 21:36:33 +0330

    Conditional Rendering Added Let's go....

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import ButtonTailwind from "./ButtonTailwind";
+import ConditionalRendering from "./ConditionalRendering";
 import Postcards from "./Postcards";
 function App() {
+  const [show, setShow] = useState(false);
+  const changer = (value) => {
+    setShow(value);
+  };
+
   return (
     <>
       <h1 className="text-5xl text-center mt-3">پروژه حامدیخواه</h1>
-      <ButtonTailwind />
-      <Postcards />
+      <section className="flex flex-col gap-10 my-10">
+        <ButtonTailwind OnChanger={changer} />
+        {show && <ConditionalRendering />}
+        <Postcards />
+      </section>
     </>
   );
 }
--- src/ButtonTailwind.jsx
+++ src/ButtonTailwind.jsx
@@ -1,15 +1,25 @@
-function ButtonTailwind() {
+function ButtonTailwind({ OnChanger }) {
   return (
     <div>
-      <div className="bg-rose-500 w-2/3 min-h-[50vh] m-auto flex flex-col justify-evenly items-center rounded-[2rem] my-11">
+      <div className="bg-rose-500 w-2/3 min-h-[50vh] m-auto flex flex-col justify-evenly items-center rounded-[2rem]">
         <h3 className="font-bold text-3xl text-white">
           تمرین تیلویند در ری اکت
         </h3>
+        <h5 className="text-[#fff] font-medium">
+          {" "}
+          بر روی دکمه ها بزن تا تغییر رو ببینی
+        </h5>
         <div className="flex gap-5">
-          <button className="border-4 border-black p-4 rounded-3xl bg-white font-medium">
+          <button
+            onClick={() => OnChanger(true)}
+            className="border-4 border-black p-4 rounded-3xl bg-white font-medium"
+          >
             من مخاطب مد نظر هستم
           </button>
-          <button className="border-4 border-black p-4 rounded-3xl bg-white font-medium">
+          <button
+            onClick={() => OnChanger(false)}
+            className="border-4 border-black p-4 rounded-3xl bg-white font-medium"
+          >
             من مخاطب مد نظر نیستم
           </button>
         </div>
