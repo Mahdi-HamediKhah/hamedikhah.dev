6b0147f8b374554d1b56a67906869c3cec2e9595
Mahdi HamediKhah <m.k.h.13679@gmail.com>
2026-02-17 17:25:35 +0330

    Private Part Added..

--- src/Postcards.jsx
+++ src/Postcards.jsx
@@ -1,22 +1,47 @@
 function Postcards() {
-    const cards = [
-        { course: "مرحله اول", id:"1", description: "توضیحات مرحله اول در این قسمت نوشته می‌شود." },
-        { course: "مرحله دوم", id:"2", description: "توضیحات مرحله دوم در این قسمت نوشته می‌شود." },
-        { course: "مرحله سوم", id:"3", description: "توضیحات مرحله سوم در این قسمت نوشته می‌شود." },
-        { course: "مرحله چهارم", id:"4", description: "توضیحات مرحله چهارم در این قسمت نوشته می‌شود." }
-    ];
+  const cards = [
+    {
+      course: "لبخندِ نیایش",
+      id: "1",
+      description:
+        "نیایشِ من، لبخندت مثل طلوعِ آرومِ صبحه؛ همون لحظه‌ای که دنیا تازه نفس می‌کشه. هر بار می‌خندی، دلم یاد می‌گیره زندگی هنوز قشنگه و من خوشبخت‌ترینم که دلیلِ این قشنگی تویی.",
+    },
+    {
+      course: "مهربونیِ بی‌مرز",
+      id: "2",
+      description:
+        "تو مهربون‌ترین اتفاقِ دنیامی، نیایش… مهربونی‌ات بی‌صدا میاد و همه‌چیز رو بهتر می‌کنه. من قدرِ همه‌ی کارهات رو می‌دونم؛ حتی اون چیزای کوچیکی که کسی نمی‌بینه، ولی من می‌بینم و هر بار بیشتر عاشقت می‌شم.",
+    },
+    {
+      course: "چشم‌های آرامش",
+      id: "3",
+      description:
+        "چشم‌هات برای من یه خونه‌ان؛ جایی که خستگی‌هام آروم می‌گیرن. نیایش عزیزم، وقتی نگاهت می‌کنم انگار همه‌ی شلوغی‌های دنیا ساکت می‌شن و فقط یه جمله می‌مونه: «کنارِ تو، امن‌ترین جای جهانه.»",
+    },
+    {
+      course: "دوست داشتنِ هر روزه",
+      id: "4",
+      description:
+        "نیایشِ من، دوست داشتنِ تو شبیه یه شعره که هر روز قشنگ‌تر می‌شه. قول نمی‌دم دنیا همیشه ساده باشه، اما قول می‌دم همیشه کنارت باشم؛ با افتخار، با احترام، با عشق… و با تمامِ قلبم بگم: ممنون که هستی.",
+    },
+  ];
   return (
-    <div className="bg-green-400 rounded-[1.5rem] p-4 m-auto w-2/3 min-h-[300px] flex flex-col justify-around items-baseline">
-      <h3 className="font-extrabold">تمرین کامپوننت های ریوزبل</h3>
-        <ul className="flex flex-col gap-3">
-            {cards.map((card) => (
-                <li key={card.id}>
-                    <h2 className="font-bold">{card.course}</h2>
-                    <p>{card.description}</p>
-                </li>
-            ))}
-        </ul>
+    <div className="bg-green-400 rounded-[1.5rem] p-4 m-auto w-2/3 min-h-[300px] flex flex-col justify-around gap-6">
+      <h3 className="font-extrabold text-2xl text-center">برای نیایشم</h3>
+      <ul className="flex flex-col gap-3">
+        {cards.map((card) => (
+          <li key={card.id}>
+            <details className="flex flex-col gap-2 border border-black p-5">
+              <summary className="font-medium  rounded py-2">
+                {card.course}
+              </summary>
+              <p className="text-justify">{card.description}</p>
+            </details>
+          </li>
+        ))}
+      </ul>
     </div>
-)}
+  );
+}
 
 export default Postcards;
